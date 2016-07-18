6bebb456f63d4306e21487682ada8cad9bfc0f52
Ítalo Fernandes <italogsfernandes@gmail.com>
2016-07-17 22:39:43 -0300

    instancio pecas

---
+++ Xadrez de Bruxo/Assets/Scripts/Controllers/TabuleiroController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabuleiroController : MonoBehaviour {
+
+	// Use this for initialization
+	private Tabuleiro tabuleiro;
+
+	public float x_unidade;
+	public float y_unidade;
+
+	public GameObject peaoP;
+	public GameObject torreP;
+	public GameObject cavaloP;
+	public GameObject bispoP;
+	public GameObject reiP;
+	public GameObject rainhaP;
+
+	public GameObject peaoB;
+	public GameObject torreB;
+	public GameObject cavaloB;
+	public GameObject bispoB;
+	public GameObject reiB;
+	public GameObject rainhaB;
+
+
+	void Start () {
+		Debug.Log ("iniciado");
+		tabuleiro = new Tabuleiro ();
+		ConvertePecas ();
+	}
+
+	private void ConvertePecas(){
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				switch (tabuleiro.posicoes[i,j]) {
+				case -1:
+					Posiciona (peaoP, i, j);
+					break;
+				case -2:
+					Posiciona (torreP, i, j);
+					break;
+				case -3:
+					Posiciona (cavaloP, i, j);
+					break;
+				case -4:
+					Posiciona (bispoP, i, j);
+					break;
+				case -5:
+					Posiciona (rainhaP, i, j);
+					break;
+				case -6:
+					Posiciona (reiP, i, j);
+					break;
+				case 1:
+					Posiciona (peaoB, i, j);
+					break;
+				case 2:
+					Posiciona (torreB, i, j);
+					break;
+				case 3:
+					Posiciona (cavaloB, i, j);
+					break;
+				case 4:
+					Posiciona (bispoB, i, j);
+					break;
+				case 5:
+					Posiciona (rainhaB, i, j);
+					break;
+				case 6:
+					Posiciona (reiB, i, j);
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		Debug.Log ("Posicionadas");
+	}
+
+	private void Posiciona(GameObject peca, int x, int y) {
+		GameObject element = (GameObject) Instantiate (peca,
+			new Vector3 ((y-4) * y_unidade, (x - 4) * x_unidade, -0.2f),
+			Quaternion.identity);
+		element.name = peca.name + "("+x.ToString()+", " + y.ToString() + ")";
+		element.transform.SetParent (this.transform, true);
+	}
+}
--- Xadrez de Bruxo/Assets/Scripts/Models/Tabuleiro.cs
+++ Xadrez de Bruxo/Assets/Scripts/Models/Tabuleiro.cs
@@ -4,71 +4,25 @@ using System;
 
 public class Tabuleiro {
 
+	//public enum Pecas{ peao, torre, cavalo, bispo, rei, rainha};
 
-	public Setdepecas brancas;
-	public Setdepecas pretas;
-
-	public UInt64 ocupadas {
-		get{
-			return (brancas.ocupadas | pretas.ocupadas);
-		}
-		private set{ ;}
-	}
-
-	public UInt64 livres {
-		get{ 
-			return (~ocupadas);
-		}
-		set{ ;}
-	}
+	public int[,] posicoes { get; set; }
+	
+	public int[,] movimentos = new int[8,8];
 
 	public Tabuleiro() {
-		this.brancas = new Setdepecas (this, 0);
-		this.pretas = new Setdepecas (this, 1);
+		PosicoesIniciais ();
+
 	}
-
-	public void Mover(Vector2 Inicio, Vector2 Final){
-		UInt64 inicio = 1;
-		inicio = inicio << Convert.ToInt32(Inicio.x);
-		inicio = inicio << Convert.ToInt32(Inicio.y * 8);
-
-		UInt64 final = 1;
-		final = final << Convert.ToInt32(Final.x);
-		final = final << Convert.ToInt32(Final.y * 8);
-
-//		var quem = GetPecaAt (inicio);
+	public void PosicoesIniciais(){
+		this.posicoes = new int[,] { 
+			{ 2, 3, 4, 5, 6, 4, 3, 2},
+			{ 1, 1, 1, 1, 1, 1, 1, 1},
+			{ 0, 0, 0, 0, 0, 0, 0, 0},
+			{ 0, 0, 0, 0, 0, 0, 0, 0},
+			{ 0, 0, 0, 0, 0, 0, 0, 0},
+			{ 0, 0, 0, 0, 0, 0, 0, 0},
+			{-1,-1,-1,-1,-1,-1,-1,-1},
+			{-2,-3,-4,-5,-6,-4,-3,-2} };
 	}
-
-//	public Peca GetPecaAt(UInt64 local){
-//		if ((local & pretas.ocupadas) > 0) {
-//			if ((local & pretas.peoes.posicao) > 0) {
-//				return pretas.peoes;
-//			} else if ((local & pretas.torres.posicao) > 0) {
-//				return pretas.torres;
-//			} else if ((local & pretas.cavalos.posicao) > 0) {
-//				return pretas.cavalos;
-//			} else if ((local & pretas.bispos.posicao) > 0) {
-//				return pretas.bispos;
-//			} else if ((local & pretas.rei.posicao) > 0) {
-//				return pretas.rei;
-//			} else if ((local & pretas.rainha.posicao) > 0) {
-//				return pretas.rainha;
-//			} 
-//		} else if ((local & brancas.ocupadas) > 0) {
-//			if ((local & brancas.peoes.posicao) > 0) {
-//				return brancas.peoes;
-//			} else if ((local & brancas.torres.posicao) > 0) {
-//				return brancas.torres;
-//			} else if ((local & brancas.cavalos.posicao) > 0) {
-//				return brancas.cavalos;
-//			} else if ((local & brancas.bispos.posicao) > 0) {
-//				return brancas.bispos;
-//			} else if ((local & brancas.rei.posicao) > 0) {
-//				return brancas.rei;
-//			} else if ((local & brancas.rainha.posicao) > 0) {
-//				return brancas.rainha;
-//			} 
-////		}
-//
-//	}
 }
